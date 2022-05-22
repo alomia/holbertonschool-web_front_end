05e2f6f9730f172e172d9bce30fd5a2367ddf1df
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-05-21 23:47:12 -0500

    Prime numbers & timing execution

--- 0x0C-Javascript_advanced/9-prime.js
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -1,8 +1,8 @@
 function countPrimeNumbers() {
-    return 25;
+  return 25;
 }
 
-const t0 = performance.now()
+const t0 = performance.now();
 countPrimeNumbers();
-const t1 = performance.now()
+const t1 = performance.now();
 console.log(`Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds`);
